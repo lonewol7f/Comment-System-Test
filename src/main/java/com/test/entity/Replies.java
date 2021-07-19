2076d1d78ff678459844619ecd886c4e602b130f
Kalana <kalana.madusanka26765@gmail.com>
2021-07-19 21:24:18 +0530

    Create Replies.java

---
+++ src/main/java/com/test/entity/Replies.java
@@ -0,0 +1,70 @@
+package com.test.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by Kalana on 19/07/2021
+ */
+
+
+@Entity
+@Table(name = "replies")
+public class Replies {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "reply")
+    private String reply;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH,
+            CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "comment_id")
+    private Comments comment;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH,
+            CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    public Replies() {
+    }
+
+    public Replies(String reply) {
+        this.reply = reply;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getReply() {
+        return reply;
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+
+    public Comments getComment() {
+        return comment;
+    }
+
+    public void setComment(Comments comment) {
+        this.comment = comment;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+}
