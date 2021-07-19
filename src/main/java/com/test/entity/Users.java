0375bdfbdbb89138103791f7e3d1d9338dd610a8
Kalana <kalana.madusanka26765@gmail.com>
2021-07-19 21:24:25 +0530

    Create Users.java

---
+++ src/main/java/com/test/entity/Users.java
@@ -0,0 +1,89 @@
+package com.test.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by Kalana on 19/07/2021
+ */
+
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Comments> comments;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Replies> replies;
+
+    public Users() {
+    }
+
+    public Users(int id, String username, String name) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Comments> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comments> comments) {
+        this.comments = comments;
+    }
+
+    public List<Replies> getReplies() {
+        return replies;
+    }
+
+    public void setReplies(List<Replies> replies) {
+        this.replies = replies;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
