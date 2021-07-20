5d2d740c8803c62b7881bda9328bec884d23b67f
Kalana <kalana.madusanka26765@gmail.com>
2021-07-20 18:10:56 +0530

    Create CommentService.java

---
+++ src/main/java/com/test/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.test.service;
+
+import java.util.List;
+
+/**
+ * Created by Kalana on 20/07/2021
+ */
+
+public interface CommentService {
+
+    List<List<Object>> getComments();
+
+}
