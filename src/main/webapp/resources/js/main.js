4b32f93b727ad7dd46dc66f39c1c297426e7a8d1
Kalana <kalana.madusanka26765@gmail.com>
2021-07-23 18:59:15 +0530

    Update main.js

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,47 +1,57 @@
-function doSomething() {
+function loadData() {
 
-    $.getJSON("hello",
-        {COMMENT: $('#commentBox').val()},
+    $.getJSON("comments/load",
         function (data) {
             $('#results').text('');
-            $('#commentBox').val('');
 
             for (let index in data) {
-                $('#results').append('<div style="margin-left: 5%;margin-right: 5%;margin-bottom: 3%" class="row">\n' +
-                    '    <div class="item col-12 border border-2 rounded mb-2">\n' +
-                    '        <p>' + data[index].comment +
-                    '        </p>\n' +
-                    '    </div>\n' +
-                    '    <div>\n' +
-                    '        <button onclick="helloWorld()" class="btn btn-info">Reply</button>\n' +
-                    '    </div>\n' +
-                    '</div>')
-                // $('#results').append(data[index].comment + '<br>');
-                // for (let reply in data[index]["replies"]) {
-                //     $('#results').append('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + data[index].replies[reply].reply + '<br>');
-                // }
-                // $('#results').append('<br>');
-
-            }
+                $('#results').append('<div style="margin-left: 5%;margin-right: 5%;margin-bottom: 3%;margin-top: 3%" class="border border-2 rounded mb-3">\n' +
+                    '    <div style="margin-left: 5%;margin-right: 5%;margin-bottom: 3%;margin-top: 3%" class="row">\n' +  //comment start
+                            data[index].user.username + '\n' +
+                    '        <div class="item col-12 border border-1 rounded mb-2">\n' +
+                    '            <p>' + data[index].comment +
+                    '            </p>\n' +
+                    '        </div>\n' +
+                    '        <div id="rep' + index + '">\n' +
+                    '            <button onclick="addField(this.id)" class="btn btn-info" id="' + index + '">Reply</button>\n' +
+                    '        </div>\n' +
+                    '    </div>\n' +        // comment end
+                    '    <div id="div' + index + '"></div>\n' +
+                    '    <div id="repliesDiv' + index + '" class="mb-3 row" style="margin-left: 15%;margin-right: 5%"></div>' +
+                    '</div>');
+                for (let reply in data[index]["replies"]) {
+                    $('#repliesDiv' + index).append('' +
+                    '    <div class="item col-12 border border-1 rounded mb-2">\n' +
+                    '        <p>' + data[index]["replies"][reply].reply + '</p>\n' +
+                    '    </div>');
+                }
+            } // comment and replies loader end
+            loader();
         });
 
 }
 
 
-/*
-<div style="margin-left: 5%;margin-right: 5%;" class="row">
-    <div class="item col-12">
-        <p>The arrangement of type involves the selection of typefaces, point size, line length, leading
-            (line spacing), adjusting the spaces between groups of letters (tracking) and adjusting the
-            space between pairs of letters (kerning).
-        </p>
-    </div>
-    <div>
-        <button class="btn btn-info">Reply</button>
-    </div>
-</div>
-*/
+function addField(id) {
+    $('#rep' + id).text('');
+    $('#div' + id).append('<div class="mb-3" style="margin-left: 10%;margin-right: 5%">\n' +
+        '            <textarea class="form-control" id="replyBox" placeholder="Type your reply..." rows="3"\n' +
+        '                      style="resize: none;"></textarea>\n' +
+        '            <div class="row mt-3">\n' +
+        '                <div class="col-8"></div>\n' +
+        '                <div class="col-2"><button class="btn btn-danger" id="cancel' + id + '" onclick="cancelRep(this.id)">Cancel</button></div>\n' +
+        '                <div class="col-2"><button class="btn btn-success" id="replybtn' + id + '" onclick="sendReply(this.id)">Send</button></div>\n' +
+        '            </div>' +
+        '        </div>');
+}
 
-function helloWorld() {
-    alert('hello');
+function cancelRep(id) {
+    $('#rep' + id.substring(6)).append('<button onclick="addField(this.id)" class="btn btn-info" id="' + id.substring(6) + '">Reply</button>');
+    $('#div' + id.substring(6)).text('');
+}
+
+function sendReply(id) {
+    var reply = $("#replyBox").val();
+    cancelRep('cancel' + id.substring(8));
+    alert('Reply: ' + reply);
 }
